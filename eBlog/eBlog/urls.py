0c71b8d992fadba814e43effa85a5659570dc567
TsotneBareldze <bareladzet@gmail.com>
2021-04-03 22:18:45 +0200

    RegistrationForm

--- eBlog/eBlog/urls.py
+++ eBlog/eBlog/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('BlogApp.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
 ]
